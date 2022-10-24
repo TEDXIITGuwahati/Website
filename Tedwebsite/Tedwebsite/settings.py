47e23df292d401ea9e0cbe42c4d6d42f4ea30385
Abhiram <bingiabhiramgoud2002@gmail.com>
2022-10-24 16:30:03 +0530

    done
    done

--- Tedwebsite/Tedwebsite/settings.py
+++ Tedwebsite/Tedwebsite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '+l4hm@bcff)7v0xk17&bzjtyj*%_)p4jfsd&3!oirvhnaiubho'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tedxiitguwahati.in','ec2-65-2-125-47.ap-south-1.compute.amazonaws.com', '127.0.0.1']
+ALLOWED_HOSTS = ['tedxiitguwahati.in','ec2-65-2-125-47.ap-south-1.compute.amazonaws.com', '127.0.0.1','ec2-18-212-62-215.compute-1.amazonaws.com','www.tedxiitguwahati.in']
 
 # Application definition
 
